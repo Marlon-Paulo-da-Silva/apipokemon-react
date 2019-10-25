7835ebbc71d69c9fefe0aa44f3c07b2f52914946
Marlon Paulo <marlon.paulo.silva@outlook.com>
2019-10-25 00:42:55 -0300

    adicionando axios e conectando a api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+import PokemonList from "./PokemonList";
+import  axios  from "axios";
+
 
 function App() {
+  const [pokemon, setPokemon] = useState([])
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+
+  useEffect(() => {
+    axios.get(currentPageUrl).then(res => {
+      setPokemon(res.data.results.map(p => p.name))
+    })
+  }, [currentPageUrl])
+
   return (
-    null
+    <PokemonList pokemon={pokemon}/>
   );
 }
 

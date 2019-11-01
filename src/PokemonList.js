8aeeffbd865506353b176738967d1e8ffc9cc38d
Marlon Paulo <marlon.paulo.silva@outlook.com>
2019-11-01 03:43:23 -0300

    estilizadinha na lista

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function PokemonList({ pokemon }) {
     return (
         <div>
-            {pokemon.map(p => (
-                <div key={p}>{p}</div>
+           {pokemon.map(p => (
+                <div style={{border: 'solid 2px', borderRadius: '10px', width: '200px', display:'flex', padding: '10px', margin: '10px auto', justifyContent: 'center' }} key={p}>{p}</div>
             ))}
         </div>
     )
